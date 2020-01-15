634f5caf120b8491f9c36cce8e00567b913b898d
Suraj Shrestha <syuraj@gmail.com>
2020-01-15 01:02:58 -0500

    cleaning up chart

--- src/components/stock/StockForecast.js
+++ src/components/stock/StockForecast.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Row, Col, Card, CardHeader, CardBody } from 'shards-react'
+import { Card, CardHeader, CardBody } from 'shards-react'
 
-import RangeDatePicker from '../common/RangeDatePicker'
 import Chart from '../../utils/chart'
 
 class StockForecast extends React.Component {
@@ -17,16 +16,16 @@ class StockForecast extends React.Component {
 			...{
 				responsive: true,
 				legend: {
-					position: 'top',
+					display: false
 				},
 				elements: {
 					line: {
 						// A higher value makes the line look skewed at this ratio.
-						tension: 0.3,
+						tension: 0.3
 					},
 					point: {
-						radius: 0,
-					},
+						radius: 0
+					}
 				},
 				scales: {
 					xAxes: [
@@ -36,9 +35,9 @@ class StockForecast extends React.Component {
 								callback(tick, index) {
 									// Jump every 7 values on the X axis labels to avoid clutter.
 									return index % 7 !== 0 ? '' : tick
-								},
-							},
-						},
+								}
+							}
+						}
 					],
 					yAxes: [
 						{
@@ -50,28 +49,28 @@ class StockForecast extends React.Component {
 									}
 									// Format the amounts using Ks for thousands.
 									return tick > 999 ? `${(tick / 1000).toFixed(1)}K` : tick
-								},
-							},
-						},
-					],
+								}
+							}
+						}
+					]
 				},
 				hover: {
 					mode: 'nearest',
-					intersect: false,
+					intersect: false
 				},
 				tooltips: {
 					custom: false,
 					mode: 'nearest',
-					intersect: false,
-				},
+					intersect: false
+				}
 			},
-			...this.props.chartOptions,
+			...this.props.chartOptions
 		}
 
 		const BlogUsersOverview = new Chart(this.canvasRef.current, {
-			type: 'LineWithLine',
+			type: 'line',
 			data: this.props.chartData,
-			options: chartOptions,
+			options: chartOptions
 		})
 
 		// They can still be triggered on hover.
@@ -91,11 +90,6 @@ class StockForecast extends React.Component {
 					<h6 className="m-0">{title}</h6>
 				</CardHeader>
 				<CardBody className="pt-0">
-					<Row className="border-bottom py-2 bg-light">
-						<Col sm="6" className="d-flex mb-2 mb-sm-0">
-							<RangeDatePicker />
-						</Col>
-					</Row>
 					<canvas height="120" ref={this.canvasRef} style={{ maxWidth: '100% !important' }} />
 				</CardBody>
 			</Card>
@@ -106,7 +100,7 @@ class StockForecast extends React.Component {
 StockForecast.propTypes = {
 	title: PropTypes.string,
 	chartData: PropTypes.object,
-	chartOptions: PropTypes.object,
+	chartOptions: PropTypes.object
 }
 
 StockForecast.defaultProps = {
@@ -114,83 +108,21 @@ StockForecast.defaultProps = {
 		labels: Array.from(new Array(30), (_, i) => (i === 0 ? 1 : i)),
 		datasets: [
 			{
-				label: 'Current Month',
-				fill: 'start',
-				data: [
-					500,
-					800,
-					320,
-					180,
-					240,
-					320,
-					230,
-					650,
-					590,
-					1200,
-					750,
-					940,
-					1420,
-					1200,
-					960,
-					1450,
-					1820,
-					2800,
-					2102,
-					1920,
-					3920,
-					3202,
-					3140,
-					2800,
-					3200,
-					3200,
-					3400,
-					2910,
-					3100,
-					4250,
-				],
+				label: 'Price',
+				fill: false,
+				data: [500, 800, 320, 180, 240, 320, 230, 650],
 				backgroundColor: 'rgba(0,123,255,0.1)',
 				borderColor: 'rgba(0,123,255,1)',
 				pointBackgroundColor: '#ffffff',
 				pointHoverBackgroundColor: 'rgb(0,123,255)',
 				borderWidth: 1.5,
 				pointRadius: 0,
-				pointHoverRadius: 3,
+				pointHoverRadius: 3
 			},
 			{
-				label: 'Past Month',
-				fill: 'start',
-				data: [
-					380,
-					430,
-					120,
-					230,
-					410,
-					740,
-					472,
-					219,
-					391,
-					229,
-					400,
-					203,
-					301,
-					380,
-					291,
-					620,
-					700,
-					300,
-					630,
-					402,
-					320,
-					380,
-					289,
-					410,
-					300,
-					530,
-					630,
-					720,
-					780,
-					1200,
-				],
+				label: 'Prediction',
+				fill: false,
+				data: [380, 430, 120, 230, 410, 740, 472, 219],
 				backgroundColor: 'rgba(255,65,105,0.1)',
 				borderColor: 'rgba(255,65,105,1)',
 				pointBackgroundColor: '#ffffff',
@@ -199,10 +131,10 @@ StockForecast.defaultProps = {
 				borderWidth: 1,
 				pointRadius: 0,
 				pointHoverRadius: 2,
-				pointBorderColor: 'rgba(255,65,105,1)',
-			},
-		],
-	},
+				pointBorderColor: 'rgba(255,65,105,1)'
+			}
+		]
+	}
 }
 
 export default StockForecast
